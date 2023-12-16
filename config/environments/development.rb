60fd48647bd8c4463ba5753b98f69c1f306f8868
ebiten1205 <ebiten.1205@gmail.com>
2023-12-16 13:02:43 +0000

    made controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "98c02e2532f54e2cb7d71fe5107411e5.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
 end
