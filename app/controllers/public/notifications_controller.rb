3114ff4d3951739a43f79f2a46745ce9f9f1efd6
ebiten1205 <ebiten.1205@gmail.com>
2024-04-01 06:03:18 +0000

    edit notification controller

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,7 +2,6 @@ class Public::NotificationsController < ApplicationController
   
   def index
     @notifications = current_customer.notifications.order(created_at: :desc)
-    
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
