84211d973d6f9bf941b1dc2927751f58659c496c
ebiten1205 <ebiten.1205@gmail.com>
2024-05-23 10:40:29 +0000

    edit application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_admin!, if: :admin_url
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # protected
@@ -6,4 +7,8 @@ class ApplicationController < ActionController::Base
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   # end
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
+  
 end
