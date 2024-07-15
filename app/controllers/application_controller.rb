5974f5282031861465b16a5d89f835b292544269
ebiten1205 <ebiten.1205@gmail.com>
2024-07-15 13:55:35 +0000

    edit html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   # end
+  
   def admin_url
     request.fullpath.include?("/admin")
   end
-  
 end
