cd00fd73b149517cd246347ba40550a3b12109a6
ebiten1205 <ebiten.1205@gmail.com>
2024-07-16 13:12:18 +0000

    start javascript

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,3 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+console.log('test');
+console.log(1+1);
