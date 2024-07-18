dcea603cc9dc682b4ea38a5d5c20b14945c0e689
ebiten1205 <ebiten.1205@gmail.com>
2024-07-18 12:41:29 +0000

    study javaScript

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -7,3 +7,10 @@ module.exports = environment.toWebpackConfig()
 console.log('test');
 console.log(1+1);
 window.alert('おはようございます。');
+document.getElementById('choice').textContent = new Date();
+console.log(document.getElementById('choice').textContent);
+if(window.confirm('ゲームスタート！準備はいい？')) {
+  console.log('ゲームを開始します。');
+} else {
+  console.log('ゲームを終了します。');
+}
