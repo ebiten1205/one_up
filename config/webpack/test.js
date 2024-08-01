af6979f2e3b715fdb10834746201e44f64c7425a
ebiten1205 <ebiten.1205@gmail.com>
2024-08-01 10:01:34 +0000

    js function Math

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -112,5 +112,15 @@ if(hour < 12) {
   ampm = 'p.m.';
 }
 
+document.getElementById('pi').textContent = Math.PI;
+document.getElementById('floor').textContent = Math.floor(Math.PI);
+
+function point(num, digit) {
+  const mover = 10 ** digit;
+  return Math.floor(num * mover) / mover;
+}
+
+document.getElementById('output').textContent = point(Math.PI, 2);
+
 </script>
 */
