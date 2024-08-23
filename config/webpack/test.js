cb7e62e993fcb74301fbcfad5c9a2512ef5fdff8
ebiten1205 <ebiten.1205@gmail.com>
2024-08-23 12:03:32 +0000

    edit test.js

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -23,6 +23,7 @@ console.log(2+2);
 console.log(1+1);
 console.log(3+3);
 console.log('4+4');
+window.alert('ようこそ！');
 window.alert('こんにちは！');
 window.alert('おはようございます。');
 window.alert('おはすみなさい。');
